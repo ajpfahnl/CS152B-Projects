1c095fa308fbe3ec9d1666b2a65456ffe0edaf94
puvali <59841962+puvali@users.noreply.github.com>
2021-10-06 11:31:00 -0700

    Update lls_tb.v

--- lab1/src/lls_tb.v
+++ lab1/src/lls_tb.v
@@ -29,11 +29,13 @@ module lls_tb;
 
 	// Outputs
 	wire [15:0] Y;
+	wire OF;
 
 	// Instantiate the Unit Under Test (UUT)
 	left_logic_shift uut (
 		.A(A), 
-		.Y(Y)
+		.Y(Y),
+		.OF(OF)
 	);
 
 	initial begin
