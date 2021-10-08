57f60ead5e50aa5f89b050261e4260071412b14d
wahbehj18 <wahbehj18@g.ucla.edu>
2021-10-08 11:46:16 -0700

    addbit.v

---
+++ lab1/src/addbit.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:43:30 10/08/2021 
+// Design Name: 
+// Module Name:    addbit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module addbit(a,b,ci,sum,co);
+	input a;
+	input b;
+	input ci;
+	output sum;
+	output co;
+	
+	wire x,y,z,v;
+	
+	xor(x,a,b);
+	xor(Sum,x,ci);
+	and(y,a,b);
+	and(z,b,ci);
+	and(v,a,ci);
+	or(co,y,z,v);
+
+endmodule
