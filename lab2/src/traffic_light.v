4cac39199192d094802acea2e1c26cd09a1ca28f
ajpfahnl <ajpfahnl@gmail.com>
2021-10-18 17:28:01 +0000

    add FSM skeleton

--- lab2/src/traffic_light.v
+++ lab2/src/traffic_light.v
@@ -28,6 +28,31 @@ module traffic_light(
 	 input rst
     );
 	 
+	 reg [4:0] state;
+	 reg [4:0] next_state;
 	 
+	 parameter G1=5'd0, G2=5'd1, G3=5'd2, YR=5'd3, R1=5'd4, RG1=5'd5, RG2=5'd6, RY=5'd7;
+	 
+	 always@ (state) begin
+		case(state)
+			G1: begin
+			end
+			G2: begin
+			end
+			G3: begin
+			end
+			YR: begin
+			end
+			R1: begin
+			end
+			RG1: begin
+			end
+			RG2: begin
+			end
+			RY: begin
+			end
+			default: next_state <= G1;
+		endcase
+	 end
 
 endmodule
